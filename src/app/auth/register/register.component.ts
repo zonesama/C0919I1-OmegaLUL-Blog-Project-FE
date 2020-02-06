4ba0cdcbb3fd6ac783fc0e95f33d7ad7297bf6c5
zonesama <trannhatthanh31@gmail.com>
2020-02-06 11:54:51 +0700

    fix up UI

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -27,7 +27,9 @@ export class RegisterComponent implements OnInit {
       name: ['', Validators.required],
       username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      avatar: [''],
+      avatar: ['', Validators.required],
+      gender: ['', Validators.required],
+      dob: ['', Validators.required],
       pwGroup: this.fb.group({
         password: ['', [Validators.required, Validators.minLength(6)]],
         confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
@@ -42,7 +44,8 @@ export class RegisterComponent implements OnInit {
   onSubmit() {
     const signUpForm = new SignUpForm(0, this.registerForm.get('name').value.toString(), this.registerForm.get('username').value.toString(),
       this.registerForm.get('email').value.toString(), this.registerForm.get('pwGroup').get('password').value.toString(),
-      this.registerForm.get('avatar').value.toString(), ['user']);
+      this.registerForm.get('avatar').value.toString(), this.registerForm.get('dob').value, this.registerForm.get('gender').value,
+      ['user']);
     this.authService.signUp(signUpForm).subscribe(data => {
       this.result = data.message;
       alert(this.result);
--- src/app/auth/sign-up-form.ts
+++ src/app/auth/sign-up-form.ts
@@ -5,16 +5,20 @@ export class SignUpForm {
   email: string;
   password: string;
   avatar: string;
+  dob: Date;
+  gender: string;
   role: string[];
 
-
-  constructor(id: number, name: string, username: string, email: string, password: string, avatar: string, role: string[]) {
+  constructor(id: number, name: string, username: string, email: string, password: string, avatar: string, dob: Date,
+              gender: string, role: string[]) {
     this.id = id;
     this.name = name;
     this.username = username;
     this.email = email;
     this.password = password;
     this.avatar = avatar;
+    this.dob = dob;
+    this.gender = gender;
     this.role = role;
   }
 }
